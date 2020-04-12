4abf7805224a2758acaf0b0c986148fc73820141
Michelle Morin <morin2m@gmail.com>
2020-04-12 00:48:01 -0700

    update responsiveness and add portfolio content

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+
+
+});
