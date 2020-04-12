0f0e118a45bdfa5576e951d60e88fe668e27556e
Michelle Morin <morin2m@gmail.com>
2020-04-12 01:10:59 -0700

    add scripts for collapsible content, update skillset styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-
-
+  $("nav button").click(function() {
+    $(".collapse").collapse();
+  });
+  $("#headingOne").click(function() {
+    $(".collapse").collapse();
+  });
+  $("#headingTwo").click(function() {
+    $(".collapse").collapse();
+  });
+  $("#headingThree").click(function() {
+    $(".collapse").collapse();
+  });
+  $("#headingFour").click(function() {
+    $(".collapse").collapse();
+  });
+  $("#headingFive").click(function() {
+    $(".collapse").collapse();
+  });
 });
