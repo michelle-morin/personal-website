e0e1ec1cc1118cae1fff5ebde1bacd8db1325e0d
Michelle Morin <morin2m@gmail.com>
2020-04-13 00:30:02 -0700

    adjust indentation

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
   });
   $("nav button").click(function() {
