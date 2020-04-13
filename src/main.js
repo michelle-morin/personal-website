f86eab0963de6c02b587c52c146d3770a7589ad7
Michelle Morin <morin2m@gmail.com>
2020-04-13 00:29:12 -0700

    add smooth scrolling script

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
   $("nav button").click(function() {
     $(".collapse").collapse();
   });
